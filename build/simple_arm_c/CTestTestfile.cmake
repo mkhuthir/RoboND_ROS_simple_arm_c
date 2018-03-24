f366483c8d0d8169c2799ec8432b77a5d61ef548
mkhuthir <mkhuthir@gmail.com>
2018-03-24 20:00:26 +0400

    first push

---
+++ build/simple_arm_c/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mkhuthir/catkin_ws/src/simple_arm_c
+# Build directory: /home/mkhuthir/catkin_ws/build/simple_arm_c
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
